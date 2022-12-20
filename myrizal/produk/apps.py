917c7a4cda089a30846446eb0491ff9f9840522a
rizal alfian <tidrizal3@gmail.com>
2022-12-21 05:43:55 +0700

    menambah templates

--- myrizal/produk/apps.py
+++ myrizal/produk/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BarangConfig(AppConfig):
+class ProdukConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'barang'
+    name = 'produk'
